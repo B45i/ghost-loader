333e8d768281d4da36394afe9402bb3ef9215ea5
B45i <amalshajan2011@gmail.com>
2021-08-02 13:53:54 +0530

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Navbar from './components/Navbar';
 import Demo from './components/Demo';
-import Ghost from './shared/components/Ghost';
 
 import './App.css';
 
@@ -9,20 +8,6 @@ function App() {
         <div>
             <Navbar />
             <Demo />
-            {/* <Ghost
-                type="circle"
-                isLoading={true}
-                height="3rem"
-                width="3rem"
-            ></Ghost>
-
-            <Ghost
-                isLoading={true}
-                height="1rem"
-                width="100%"
-                count={4}
-            ></Ghost>
-            <Ghost isLoading={true} height="1rem" width="50%"></Ghost> */}
         </div>
     );
 }
